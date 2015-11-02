83b25688343647a7a14fd44d40cbd9d5c4765dfc
Thapelo <me@tsotetsithapelo.co.za>
2015-11-02 19:19:01 +0200

    add more states and tests

---
+++ go-app.js
@@ -0,0 +1,177 @@
+// WARNING: This is a generated file.
+//          If you edit it you will be sad.
+//          Edit src/app.js instead.
+
+var go = {};
+go;
+
+go.app = function() {
+    var vumigo = require('vumigo_v02');
+    var App = vumigo.App;
+    var Choice = vumigo.states.Choice;
+    var ChoiceState = vumigo.states.ChoiceState;
+    var EndState = vumigo.states.EndState;
+    var FreeText = vumigo.states.FreeText;
+
+    var GoApp = App.extend(function(self) {
+        App.call(self, 'states:main');
+
+        self.states.add('states:main', function(name) {
+            return new ChoiceState(name, {
+                question: 'Bibi Cash & Carry \n Choose a language:',
+
+                choices: [
+                    new Choice('states:main_english', 'English'),
+                    new Choice('states:main_sesotho','Sesotho'),
+                    new Choice('states:main_zulu','IsiZulu'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:main_english', function(name) {
+            return new ChoiceState(name, {
+                question: 'Bibi Cash & Carry',
+
+                choices: [
+                    new Choice('states:join_funeral_scheme', 'Join Funeral Scheme'),
+                    new Choice('states:join_grocery_scheme','Join Grocery Scheme'),
+                    new Choice('states:special_promotion', 'This month special'),
+                    new Choice('states:competitions', 'Current Competitions'),
+                    new Choice('states:contact','Our branches'),
+                    new Choice('states:about', 'About Bibi'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:main_sesotho', function(name) {
+            return new ChoiceState(name, {
+                question: 'Bibi Cash & Carry\nSesotho menu is currently not available',
+
+                choices: [
+                    new Choice('states:main_english', 'English Version'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:main_zulu', function(name) {
+            return new ChoiceState(name, {
+                question: 'Bibi Cash & Carry\nIsiZulu menu is currently not available',
+
+                choices: [
+                    new Choice('states:main_english', 'English Version'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });     
+
+        self.states.add('states:join_funeral_scheme', function(name){
+            return new FreeText(name,{
+                question: 'Enter your name, a representative will call you back shortly: ',
+                next: function(content){
+
+                    self.contact.extra.join = content;
+
+                    return self.im.contacts.save(self.contact).then(function(){
+                        return "states:exit";
+                    });
+                }
+            });
+        });
+
+        self.states.add('states:join_grocery_scheme', function(name){
+            return new FreeText(name,{
+                question: 'Enter your name, a representative will call you back shortly: ',
+                next: function(content){
+
+                    self.contact.extra.join = content;
+
+                    return self.im.contacts.save(self.contact).then(function(){
+                        return "states:exit";
+                    });
+                }
+            });
+        });
+
+        self.states.add('states:special_promotion', function(name) {
+            return new EndState(name, {
+                text: 'Ubuntu 12.5Kg: R59.99\nSunlight 2in1: R33.99\nSasko Cake Flour 12.5Kg: R72.99\nVisit the store for more..',
+                next: 'states:exit'
+            });
+        });
+
+        self.states.add('states:competitions', function(name){
+            return new ChoiceState(name,{
+                question: 'Current Competitions',
+
+                choices: [
+                    new Choice('states:competition_car', 'Festive Car Competition'),
+                    new Choice('states:competition_monthly', 'Monthly Grocery Competition'),
+                    new Choice('states:main_english', 'Back to Main menu'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:competition_car', function(name){
+            return new ChoiceState(name,{
+                question: '2015 Festive Car Competition\n\nPurchase Grocery worth more thanR 400\nand stand a chance to WIN a car\n',
+ 
+                choices: [
+                    new Choice('states:competition_monthly', 'Monthly Grocery Competition'),
+                    new Choice('states:main_english', 'Back to Main menu'),
+                    new Choice('states:exit', 'Exit')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:competition_monthly', function(name){
+            return new EndState(name,{
+                text: 'Win a Busket filled with Grocery every month by\nparticipating on out weekly surveys\nListen to Qwaqwa Radio for more details.',
+                next: 'states:main'
+            });
+        });
+
+        self.states.add('states:exit', function(name) {
+            return new EndState(name, {
+                text: 'Demo by Textily (Pty) Ltd\nTsotetsi Thapelo\n0835504933',
+                next: 'states:main'
+            });
+        });
+    });
+
+    return {
+        GoApp: GoApp
+    };
+}();
+
+go.init = function() {
+    var vumigo = require('vumigo_v02');
+    var InteractionMachine = vumigo.InteractionMachine;
+    var GoApp = go.app.GoApp;
+
+
+    return {
+        im: new InteractionMachine(api, new GoApp())
+    };
+}();
---
+++ test/app.test.js
@@ -0,0 +1,179 @@
+var vumigo = require('vumigo_v02');
+var fixtures = require('./fixtures');
+var AppTester = vumigo.AppTester;
+
+
+describe("app", function() {
+    describe("GoApp", function() {
+        var app;
+        var tester;
+
+        beforeEach(function() {
+            app = new go.app.GoApp();
+
+            tester = new AppTester(app);
+
+            tester
+                .setup.config.app({
+                    name: 'test_app'
+                })
+                .setup(function(api) {
+                    fixtures().forEach(api.http.fixtures.add);
+                });
+        });
+
+        describe("when the user starts a session", function(){
+            it("should present the language menu", function() {
+                return tester
+                    .start()
+                    .check.interaction({
+                        state: 'states:main',
+                        reply: [
+                            'Bibi Cash & Carry \n Choose a language:',
+                            '1. English',
+                            '2. Sesotho',
+                            '3. IsiZulu',
+                            '4. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user ask to see the english version", function(){
+            it("should present the english menu", function() {
+                return tester
+                    .setup.user.state('states:main')
+                    .input('1')
+                    .check.interaction({
+                        state: 'states:main_english',
+                        reply: [
+                            'Bibi Cash & Carry',
+                            '1. Join Funeral Scheme',
+                            '2. Join Grocery Scheme',
+                            '3. This month special',
+                            '4. Current Competitions',
+                            '5. Our branches',
+                            '6. About Bibi',
+                            '7. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user ask to see sesotho menu", function(){
+            it("should present the sesotho main menu", function() {
+                return tester
+                    .setup.user.state('states:main')
+                    .input('2')
+                    .check.interaction({
+                        state: 'states:main_sesotho',
+                        reply: [
+                            'Bibi Cash & Carry\nSesotho menu is currently not available',
+                            '1. English Version',
+                            '2. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user ask to see isizulu menu", function(){
+            it("should present the isizulu main menu", function() {
+                return tester
+                    .setup.user.state('states:main')
+                    .input('3')
+                    .check.interaction({
+                        state: 'states:main_zulu',
+                        reply: [
+                            'Bibi Cash & Carry\nIsiZulu menu is currently not available',
+                            '1. English Version',
+                            '2. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user ask to see the competition menu", function(){
+            it("should present the competition menu", function() {
+                return tester
+                    .setup.user.state('states:main_english')
+                    .input('4')
+                    .check.interaction({
+                        state: 'states:competitions',
+                        reply: [
+                            'Current Competitions',
+                            '1. Festive Car Competition',
+                            '2. Monthly Grocery Competition',
+                            '3. Back to Main menu',
+                            '4. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+
+        describe("when the user asks to see the special menu", function() {
+            it("should present the special and return to main menu", function() {
+                return tester
+                    .setup.user.state('states:main_english')
+                    .input('3')
+                    .check.interaction({
+                        state: 'states:special_promotion',
+                        reply: 'Ubuntu 12.5Kg: R59.99\nSunlight 2in1: R33.99\nSasko Cake Flour 12.5Kg: R72.99\nVisit the store for more..'
+                    })
+                    .check.reply.ends_session()
+                    .run();
+            });
+        });
+
+        describe("when the user asks to see the car competition menu", function() {
+            it("should present car competitions menu", function() {
+                return tester
+                    .setup.user.state('states:competitions')
+                    .input('1')
+                    .check.interaction({
+                        state: 'states:competition_car',
+                        reply: [
+
+                            '2015 Festive Car Competition\n\nPurchase Grocery worth more thanR 400\nand stand a chance to WIN a car\n',
+                            '1. Monthly Grocery Competition',
+                            '2. Back to Main menu',
+                            '3. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user asks to see the Monthly competition menu", function() {
+            it("should present monthly competition menu", function() {
+                return tester
+                    .setup.user.state('states:competitions')
+                    .input('2')
+                    .check.interaction({
+                        state: 'states:competition_monthly',
+                        reply: 'Win a Busket filled with Grocery every month by\nparticipating on out weekly surveys\nListen to Qwaqwa Radio for more details.'
+                    })
+                    .run();
+            });
+        });
+
+        describe("when the user asks to exit", function() {
+            it("should say thank you and end the session", function() {
+                return tester
+                    .setup.user.state('states:main_english')
+                    .input('7')
+                    .check.interaction({
+                        state: 'states:exit',
+                        reply: 'Demo by Textily (Pty) Ltd\nTsotetsi Thapelo\n0835504933'
+                    })
+                    .check.reply.ends_session()
+                    .run();
+            });
+        });
+    });
+});
